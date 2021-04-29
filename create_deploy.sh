faf4b41140b57e2b5ec8d3d3fe9d69c7089be638
Aaron Shifman <aaronrshifman@gmail.com>
2021-04-29 19:55:00 -0400

    update new style

--- create_deploy.sh
+++ create_deploy.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
 
-# only run for one instance py3.6 on linux when it's not a PR
-if [[ "$TRAVIS_PYTHON_VERSION" == "3.6" && "$TRAVIS_OS_NAME" = "linux" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-  echo "Building Docs"
-  make -C docs buildapi
-  make -C docs html
-
-  echo "Deploying docs to Github"
-  bash push.sh
-
-  exit 0
-else
-  echo "Note uploading for this config"
-  exit 0
-fi
+make -C docs buildapi
+make -C docs html
